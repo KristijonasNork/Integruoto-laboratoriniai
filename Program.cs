7f17b60de5a779bb3d3d53b51e9e51ff8ea56329
Simuciokas <maxsimass@gmail.com>
2020-02-20 15:40:52 +0200

    Atnaujinama programa
    Pridetas budas aprasyti namu darbu kieki kintamajame `n`
    Pridetas budas aprasyti tu `n` kiekio namu darbu ivertinimus skaicius tarp 1 ir 10, juos iskaitant
    Isvedimas irasytu namu darbu ivertinimu

--- Program.cs
+++ Program.cs
@@ -9,8 +9,51 @@ namespace ConsoleApp2
             string vardas = Console.ReadLine();
             string pavarde = Console.ReadLine();
 
+            int n = 0;
+            while (n == 0)
+            {
+                try
+                {
+                    n = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    n = 0;
+                }
+            }
+            int[] nd = new int[n];
+            int b = 0;
+            for (int i = 0; i < n; i++)
+            {
+                try
+                {
+                    b = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                    b = 0;
+                }
+                if (b > 0 && b < 11)
+                {
+                    nd[i] = b;
+                    Console.WriteLine("Adding");
+                }
+                else
+                {
+                    i--;
+                    Console.WriteLine("Skipping");
+                }
+            }
+
             Console.WriteLine("V ir P: " + vardas + " " + pavarde);
 
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("ND" + i + 1 + ": " + nd[i]);
+            }
+            
         }
     }
 }
