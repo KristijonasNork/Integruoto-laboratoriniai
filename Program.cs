a82d9e269324ee908fcb702e7f65c30e3925e96b
Simuciokas <maxsimass@gmail.com>
2020-02-20 15:24:01 +0200

    Pagrindinė programa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = Console.ReadLine();
+            string pavarde = Console.ReadLine();
+
+            Console.WriteLine("V ir P: " + vardas + " " + pavarde);
+
+        }
+    }
+}
