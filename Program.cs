057dc5396f4afe7d400a571f551226b00c72e2b5
Simuciokas <maxsimass@gmail.com>
2020-03-13 20:40:00 +0200

    Atnaujintas kodas
    Pridėta nauja klasė Studentas kuriame saugomi duomenys:
     - Vardas
     - Pavardė
     - Namų darbų įvertinimai
     - Egzamino įvertinimas
     - Mediana
     - Vidurkis
     - Namų darbų kiekis
    su metodais:
     - Sukurti Studentas objektą
     - Paskaičiuoti vidurkį
     - Paskaičiuoti medianą
     - Gauti galutinį įvertinimą pagal vidurki arba medianą
     - Gauti vardą
     - Gauti pavardę
    
    Ištrinti medianos ir vidurkių skaičiavimai Main metode.
    Pakoreguotas namų darbų įvertinimų įrašymas su List tipu, kad dinamiškai saugoti jų kiekį.
    Sukurtas studentų List objektas kuriame saugomi Studentas objektai.
    Padarytas ciklas kurti studentus iki kol gale bus parašyta "Ne"

--- Program.cs
+++ Program.cs
@@ -4,96 +4,132 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-		Console.WriteLine("Įrašykite vardą: ");
-		string vardas = Console.ReadLine();
-		Console.WriteLine("Įrašykite pavardę: ");
-		string pavarde = Console.ReadLine();
-
-		int n = 0;
-		while (n == 0)
+		public class Studentas
 		{
-			try
-			{
-				Console.WriteLine("Įrašykite namų darbų kieki: ");
-				n = Convert.ToInt32(Console.ReadLine());
+			private string vardas, pavarde;
+			private double[] ndBalai;
+			private int egz;
+			private double med;
+			private double vid;
+			private int ndSize;
+			
+			public Studentas(string vardas, string pavarde, double[] ndBalai, int egz) {
+				this.vardas = vardas;
+				this.pavarde = pavarde;
+				this.ndBalai = ndBalai;
+				this.egz = egz;
+				ndSize = ndBalai.Length;
+				vidurkis();
+				mediana();
 			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				n = 0;
+			
+			public void vidurkis() {
+				for (int i = 0; i < ndSize; i++)
+				{
+					vid += ndBalai[i];
+				}
+				vid /= ndSize;
 			}
+			
+			public void mediana() {
+				double[] ndSort = (double[])ndBalai.Clone();
+				Array.Sort(ndSort);
+				int mid = ndSize / 2;
+				med = (ndSize % 2 != 0) ? (double)ndSort[mid] : ((double)ndSort[mid] + (double)ndSort[mid - 1]) / 2;
+			}
+			
+			public double GetGalutinis(bool isVid) {
+				if (isVid)
+					return 0.3 * vid + 0.7 * egz;
+				else
+					return 0.3 * med + 0.7 * egz;
+			}
+			
+			public string GetVardas() {
+				return vardas;
+			}
+			
+			public string GetPavarde() {
+				return pavarde;
+			}
+			
 		}
-
-		double[] nd = new double[n];
-		int b = 0;
-		for (int i = 0; i < n; i++)
+		
+		public static void Main(string[] args)
 		{
-			try
-			{
-				Console.WriteLine("Įrašykite "+(i+1)+"-ojo namų darbo įvertinimą: ");
-				b = Convert.ToInt32(Console.ReadLine());
+			var studentai = new List<Studentas>();
+			string exit = "stay";
+			while (exit == "stay") {
+				Console.WriteLine("Įrašykite vardą: ");
+				string vardas = Console.ReadLine();
+				Console.WriteLine("Įrašykite pavardę: ");
+				string pavarde = Console.ReadLine();
+
+				var ndBalai = new List<double>();
+				int ndBalas = 0;
+				int ndKiekis = 0;
+				while (ndBalas != -1)
+				{
+					try
+					{
+						Console.WriteLine("Jeigu visi nd įvertinimai parašyti, rašykite '-1'");
+						Console.WriteLine("Įrašykite "+(ndKiekis++)+"-ojo namų darbo įvertinimą: ");
+						ndBalas = Convert.ToInt32(Console.ReadLine());
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.StackTrace);
+						ndBalas = 0;
+					}
+					if (ndBalas > 0 && ndBalas < 11)
+					{
+						ndBalai.Add(ndBalas);
+						Console.WriteLine("Pridėtas.");
+					}
+					else if (ndBalas != -1)
+					{
+						ndKiekis--;
+						Console.WriteLine("Klaidingas skaičius.");
+					}
+				}
+
+				int egz = 0;
+				while (egz == 0)
+				{
+					try
+					{
+						Console.WriteLine("Įrašykite egzamino įvertinimą: ");
+						egz = Convert.ToInt32(Console.ReadLine());
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine(e.StackTrace);
+						egz = 0;
+						Console.WriteLine("Klaidingas skaičius.");
+					}
+				}
+				studentai.Add(new Studentas(vardas, pavarde, ndBalai.ToArray(), egz));
+				Console.WriteLine("Pridėtas naujas studentas į sąrašą");
+				
+				Console.WriteLine("Ar dar sukurti studentą? Taip/Ne");
+				if (Console.ReadLine() == "Ne")
+					exit = "leave";
 			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				b = 0;
+																	 
+			Console.WriteLine("Parašykite Vid jeigu norite Vidurkių arba kitką jeigu Medianą");
+			if (Console.ReadLine() == "Vid") {
+			
+				Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Vid.)"));
+				Console.WriteLine("--------------------------------------");
+				foreach (var studentas in studentai)
+					Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", studentas.GetVardas(), studentas.GetPavarde(), studentas.GetGalutinis(true)));
 			}
-			if (b > 0 && b < 11)
-			{
-				nd[i] = b;
-				Console.WriteLine("Pridėtas.");
-			}
-			else
-			{
-				i--;
-				Console.WriteLine("Klaidingas skaičius.");
+			else {
+				Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Med.)"));
+				Console.WriteLine("--------------------------------------");
+				foreach (var studentas in studentai)
+					Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", studentas.GetVardas(), studentas.GetPavarde(), studentas.GetGalutinis(true)));
 			}
 		}
-
-		int egz = 0;
-		while (egz == 0)
-		{
-			try
-			{
-				Console.WriteLine("Įrašykite egzamino įvertinimą: ");
-				egz = Convert.ToInt32(Console.ReadLine());
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.StackTrace);
-				egz = 0;
-				Console.WriteLine("Klaidingas skaičius.");
-			}
-		}
-
-		double vid = 0;
-		for (int i = 0; i < n; i++)
-		{
-			vid += nd[i];
-		}
-		vid /= n;
-		double galutinis = 0.3 * vid + 0.7 * egz;
-
-		double[] ndSort = (double[])nd.Clone();
-		Array.Sort(ndSort);
-		int mid = n / 2;
-		double median = (n % 2 != 0) ? (double)ndSort[mid] : ((double)ndSort[mid] + (double)ndSort[mid - 1]) / 2;
-
-		Console.WriteLine("Parašykite Vid jeigu norite Vidurkių arba kitką jeigu Medianą");
-		if (Console.ReadLine() == "Vid") {
-
-			Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Vid.)"));
-			Console.WriteLine("--------------------------------------");
-			Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", vardas, pavarde, Math.Round(galutinis, 2)));
-		}
-		else {
-			Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Med.)"));
-			Console.WriteLine("--------------------------------------");
-			Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", vardas, pavarde, Math.Round(median, 2)));
-		}
-            
-        }
     }
 }
