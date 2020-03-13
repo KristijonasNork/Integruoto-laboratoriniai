288c721039b9a0f745141f421c25cb865b9bc7ce
Simuciokas <maxsimass@gmail.com>
2020-03-13 20:05:15 +0200

    Atnaujintas kodas
    Pridėta galimybė pasirinkti ar Vidurki ar Medianą rodyti

--- Program.cs
+++ Program.cs
@@ -6,79 +6,93 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            			Console.WriteLine("Įrašykite vardą: ");
-			string vardas = Console.ReadLine();
-			Console.WriteLine("Įrašykite pavardę: ");
-			string pavarde = Console.ReadLine();
+		Console.WriteLine("Įrašykite vardą: ");
+		string vardas = Console.ReadLine();
+		Console.WriteLine("Įrašykite pavardę: ");
+		string pavarde = Console.ReadLine();
 
-			int n = 0;
-			while (n == 0)
+		int n = 0;
+		while (n == 0)
+		{
+			try
 			{
-				try
-				{
-					Console.WriteLine("Įrašykite namų darbų kieki: ");
-					n = Convert.ToInt32(Console.ReadLine());
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.StackTrace);
-					n = 0;
-				}
+				Console.WriteLine("Įrašykite namų darbų kieki: ");
+				n = Convert.ToInt32(Console.ReadLine());
 			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+				n = 0;
+			}
+		}
 
-			int[] nd = new int[n];
-			int b = 0;
-			for (int i = 0; i < n; i++)
+		double[] nd = new double[n];
+		int b = 0;
+		for (int i = 0; i < n; i++)
+		{
+			try
 			{
-				try
-				{
-					Console.WriteLine("Įrašykite "+(i+1)+"-ojo namų darbo įvertinimą: ");
-					b = Convert.ToInt32(Console.ReadLine());
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.StackTrace);
-					b = 0;
-				}
-				if (b > 0 && b < 11)
-				{
-					nd[i] = b;
-					Console.WriteLine("Pridėtas.");
-				}
-				else
-				{
-					i--;
-					Console.WriteLine("Klaidingas skaičius.");
-				}
+				Console.WriteLine("Įrašykite "+(i+1)+"-ojo namų darbo įvertinimą: ");
+				b = Convert.ToInt32(Console.ReadLine());
 			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+				b = 0;
+			}
+			if (b > 0 && b < 11)
+			{
+				nd[i] = b;
+				Console.WriteLine("Pridėtas.");
+			}
+			else
+			{
+				i--;
+				Console.WriteLine("Klaidingas skaičius.");
+			}
+		}
 
-			int egz = 0;
-			while (egz == 0)
+		int egz = 0;
+		while (egz == 0)
+		{
+			try
 			{
-				try
-				{
-					Console.WriteLine("Įrašykite egzamino įvertinimą: ");
-					egz = Convert.ToInt32(Console.ReadLine());
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e.StackTrace);
-					egz = 0;
-					Console.WriteLine("Klaidingas skaičius.");
-				}
+				Console.WriteLine("Įrašykite egzamino įvertinimą: ");
+				egz = Convert.ToInt32(Console.ReadLine());
 			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.StackTrace);
+				egz = 0;
+				Console.WriteLine("Klaidingas skaičius.");
+			}
+		}
 
-			int vid = 0;
-			for (int i = 0; i < n; i++)
-			{
-				vid += nd[i];
-			}
-			vid /= n;
-			double galutinis = 0.3 * vid + 0.7 * egz;
-			
+		double vid = 0;
+		for (int i = 0; i < n; i++)
+		{
+			vid += nd[i];
+		}
+		vid /= n;
+		double galutinis = 0.3 * vid + 0.7 * egz;
+
+		double[] ndSort = (double[])nd.Clone();
+		Array.Sort(ndSort);
+		int mid = n / 2;
+		double median = (n % 2 != 0) ? (double)ndSort[mid] : ((double)ndSort[mid] + (double)ndSort[mid - 1]) / 2;
+
+		Console.WriteLine("Parašykite Vid jeigu norite Vidurkių arba kitką jeigu Medianą");
+		if (Console.ReadLine() == "Vid") {
+
 			Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Vid.)"));
 			Console.WriteLine("--------------------------------------");
-			Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", vardas, pavarde, galutinis));
+			Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", vardas, pavarde, Math.Round(galutinis, 2)));
+		}
+		else {
+			Console.WriteLine(String.Format("{0,-10} {1,-10} {2}", "Vardas", "Pavardė", "Galutinis (Med.)"));
+			Console.WriteLine("--------------------------------------");
+			Console.WriteLine(String.Format("{0,-10} {1,-10} {2,16}", vardas, pavarde, Math.Round(median, 2)));
+		}
             
         }
     }
